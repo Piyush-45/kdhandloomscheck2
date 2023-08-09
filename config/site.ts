2d48888cc0b64b5adca9aaf83192196b4f6b9a79
Piyush Tyagi <tyagipiyush2233@gmail.com>
2023-08-09 21:03:35 +0530

    first commit

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "KD HANDLOOMS",
+  description: "Description",
+  footer: [
+    { name: "Home", href: "/" },
+    // { name: "About", href: "#" },
+    // { name: "Terms & Conditions", href: "#" },
+    {name: "K D HANDLOOM STORES INFRONT OF JAIN MANDIR MOTIHARI 845401"},
+    {name:"06252-796613"}
+    // { name: "Shipping & Return Policy", href: "#" },
+    // { name: "Privacy Policy", href: "#" },
+    // { name: "FAQ", href: "#" },
+  ],
+}
