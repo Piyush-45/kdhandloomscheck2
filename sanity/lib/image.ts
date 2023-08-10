5e36b47adef0a27f7c7e801c165b8bda182c4238
Piyush Tyagi <tyagipiyush2233@gmail.com>
2023-08-10 10:03:45 +0530

    w4

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,11 +1,11 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import type { Image } from 'sanity'
 
-import { dataset, projectId } from '../env'
+
 
 const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
+  projectId: '9m26h60c' || '',
+  dataset: 'production' || '',
 })
 
 export const urlForImage = (source: Image) => {
