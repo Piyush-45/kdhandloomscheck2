851e9c2762f82f3d1af24ca82715571ca78b1087
Piyush Tyagi <tyagipiyush2233@gmail.com>
2023-08-10 11:25:30 +0530

    category_mobile_Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default async function Page({searchParams} : Props) {
               Products
             </h2>
             <div className={cn("grid grid-cols-1 gap-x-8 gap-y-10 ", products.length > 0 ? 'lg:grid-cols-4' : 'lg:grid-cols-[1fr_3fr')}>
-              <div className="hidden lg:block">
+              <div className="block">
                 <ProductFilters />
 
               </div>
